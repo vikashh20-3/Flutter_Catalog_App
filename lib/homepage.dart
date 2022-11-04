e065b4eae6990b5d818ece1f35d4a20cd502b46f
vikash <vikash711123@gmail.com>
2022-11-04 21:16:10 +0530

    navigation

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'itemwidget.dart';
 import 'login_page.dart';
 import 'drawer.dart';
+import 'models/catalog.dart';
 
 class homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Catalog App"),
-      ),
-      drawer: MyDrawer(),
-      body: Center(
-          child: Container(
-        child: new Text("Welcome "),
-      )),
-    );
+        appBar: new AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 80.0),
+            child: new Text("Catalog App"),
+          ),
+        ),
+        drawer: MyDrawer(),
+        body: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget();
+          },
+        ));
   }
 }
---
+++ lib/itemwidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:third/models/catalog.dart';
+import './models/catalog.dart';
+import 'models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  // const ItemWidget({Key key,@required this.item}):super(key:key));
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
